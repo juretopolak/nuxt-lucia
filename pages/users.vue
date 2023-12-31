e9284c14632531fb8a4c702e4daf1466f4d5ad04
Jure Topolak <jure.topolak@gmail.com>
2023-12-31 15:51:12 +0100

    Use $fetch helper instead of useFetch since it's more appropriate here.

--- pages/users.vue
+++ pages/users.vue
@@ -9,13 +9,13 @@ const columns = [
 ]
 
 async function deleteUser(id: string) {
-  const { data: deletedUser } = await useFetch(`/api/users/${id}`, { method: 'DELETE' })
+  const deletedUser = await $fetch(`/api/users/${id}`, { method: 'DELETE' })
   const toast = useToast()
 
-  if (deletedUser.value && users.value) {
-    users.value = users.value.filter(user => user.id !== deletedUser.value?.id)
+  if (deletedUser && users.value) {
+    users.value = users.value.filter(user => user.id !== deletedUser.id)
     toast.add({
-      title: `User ${deletedUser.value.name} deleted.`,
+      title: `User ${deletedUser.name} deleted.`,
       icon: 'i-heroicons-check-circle-20-solid',
       color: 'green',
     })
